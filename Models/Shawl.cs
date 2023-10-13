0e49439270a0c2bcc6e9563f32e0144825c376a9
david-pawar <135388636+david-pawar@users.noreply.github.com>
2023-10-13 10:55:01 -0400

    added Validation

--- Models/Shawl.cs
+++ Models/Shawl.cs
@@ -9,12 +9,31 @@ namespace ShawlGalore.Models
         public int Id { get; set; }
 
         //  public string Image { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Material { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Color { get; set; }
+
+
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
         public string Design { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Occasion { get; set; }
+
+        [Required]
         public string Size { get; set; }
+
+
         [Column(TypeName = "decimal(18, 2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
     }
 }
